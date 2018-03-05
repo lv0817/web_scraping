796de48a677e27160d953ef84ede9e4c65b5cc56
lv0817 <412435484@qq.com>
2018-03-05 11:16:11 +0800

    爬取百度百科
    第一版，随机跳转链接

---
+++ baidubaike.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import urllib.request
+import re
+import random
+
+base_url = 'https://baike.baidu.com'
+his = ["/item/%E7%BD%91%E7%BB%9C%E7%88%AC%E8%99%AB/5162711"]
+
+
+for i in range(20):
+    url = base_url+his[-1]
+    html = urllib.request.urlopen(url).read().decode('utf-8')
+    soup = BeautifulSoup(html,features='lxml')
+    print(i,soup.find('h1').get_text(),' url',his[-1])#h1是主标题
+
+    # find valid urls
+    sub_urls = soup.find_all("a", {"target": "_blank", "href": re.compile("/item/(%.{2})+$")})
+
+    if len(sub_urls)!=0:
+        his.append(random.sample(sub_urls,1)[0]['href'])
+        
+    else:
+        his.pop()
+print(his)
